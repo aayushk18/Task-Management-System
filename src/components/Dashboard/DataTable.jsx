aad0c2e4ac289c817765e43dc5f993219def9be0
aayushk18 <kumaraayush1807@gmail.com>
2025-05-21 18:12:00 +0530

    Update DataTable.jsx

--- src/components/Dashboard/DataTable.jsx
+++ src/components/Dashboard/DataTable.jsx
@@ -33,36 +33,11 @@ const DataTable = ({ Data }) => {
 
 
     return (
-        // <div className=' w-full '>
-        //     <div className='m-10 p-10 shadow-2xl bg-[rgb(40,40,40)] shadow-black rounded-4xl'>
-        //         {/* ---------------------- head -------------------------- */}
-
-        //         <div className='flex flex-row  inset-shadow-sm p-5 bg-red-600 inset-shadow-black rounded-2xl m-5 py-8 '>
-        //             <div className='w-full font-bold text-center'>Employee Name</div>
-        //             <div className='w-full font-bold text-center'>New Task</div>
-        //             <div className='w-full font-bold text-center'>Active Task</div>
-        //             <div className='w-full font-bold text-center'>Completed</div>
-        //             <div className='w-full font-bold text-center'>Failed</div>
-        //         </div>
-
-        //         {/* --------------------- content-------------------------- */}
-        //         <div className=' overflow-hidden  '>
-        //             {userData.slice(0, sliceValue).map((el) => {
-
-        //                 return <TaskAdminDetail key={el.name} el={el} />
-
-
-
-        //             })}
-        //         </div>
-        //         <div className='w-full text-center cursor-pointer hover:scale-110 ease-in-out transition duration-500  text-amber-300 ' onClick={() => { setTableSize() }}>{expandValue}</div>
-
-        //     </div>
-        // </div>
+       
 
 
         <div className='w-full'>
-            <div className='m-4 sm:m-6 md:m-10 p-4 sm:p-6 md:p-10 shadow-2xl bg-[rgb(40,40,40)] shadow-black rounded-3xl'>
+            <div className='m-10 sm:m-6 md:m-10 p-4 sm:p-6 md:p-10 shadow-2xl bg-[rgb(40,40,40)] shadow-black rounded-3xl'>
 
                 <div className='hidden md:flex flex-row p-5 bg-red-600 shadow-inner shadow-black rounded-2xl mb-5'>
                     <div className='w-full font-bold text-center text-white'>Employee Name</div>
